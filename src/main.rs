2343e5198697f5d3e3e084ed296f1781e956b82f
Andrew Wasilczuk <akw@scalefactory.com>
2019-12-13 00:01:35 +0000

    wrote a run function

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use std::process;
+// use std::process;
 use std::process::Command;
 use std::io::{self, Write};
+
+fn run(cmd: &str, args: &[&str]) -> Result<std::process::Output, io::Error> {
+
+    let output = Command::new(cmd).args(args).output()?;
+    Ok(output)
+
+    // {
+    //     Ok(output)  => output,
+    //     Err(e)      => {
+    //         eprintln!("muchas problemos: {}", e);
+    //         process::exit(1);
+    //     },
+    // };
+
+    // return test
+}
 
 fn main() {
     let _release = "12-RELEASE";
 
-    let test = match Command::new("/bin/lsss").args(&["/"]).output() {
-        Ok(output)  => output,
-        Err(e)      => {
-            eprintln!("muchas problemos: {}", e);
-            process::exit(1);
-        },
-    };
+    let out = run("ls", &["/"]).unwrap();
 
-    io::stdout().write_all(&test.stdout).unwrap();
+    io::stdout().write_all(&out.stdout).unwrap();
 
     // println!("{:?}", test);
 }
