7cca4d16e230ccc65259a6d5426b192be67e7cc5
Andrew Wasilczuk <akw@scalefactory.com>
2019-12-17 21:06:41 +0000

    Got rid of unncesessary function

--- src/main.rs
+++ src/main.rs
@@ -79,75 +79,17 @@ fn zfs_create_ds(ds: &str) -> GenResult<()> {
     }
 }
 
-fn zfs_create_all(sets: Vec<&str>) -> GenResult<()> {
-    for set in sets {
-        println!("Creating jail data set {}", set);
-        zfs_create_ds(&set)?;
-    };
-    Ok(())
-}
-
 fn main() {
-    let _release = "12-RELEASE";
-    let jdataset = "zroot/jails";
+    let _release = String::from("12-RELEASE");
+    let jdataset = String::from("zroot/jails");
     let bjdataset = format!("{}/basejail", &jdataset);
 
-    zfs_create_all(vec![&jdataset, &bjdataset]).unwrap_or_else(|err| {
-        eprintln!("ERROR: {}", err);
-        process::exit(1);
-    });
+    for set in vec![&jdataset, &bjdataset] {
+        println!("Creating jail data set {}", set);
+        zfs_create_ds(&set).unwrap_or_else(|err| {
+            eprintln!("ERROR: {}", err);
+            process::exit(1);
+        });
+    };
 
-
-
-    // let mut zfs = Command::new("zfs");
-    // zfs.arg("list").arg(&bjdataset);
-
-    // run(&mut zfs).unwrap_or_else(|err| {
-    //     eprintln!("ERROR: {}", err);
-    //     process::exit(1);
-    // });
-
-    // process::exit(0);
-    // let output = Command::new("zfs").args(&["list", &bjdataset]).output().unwrap();
-
-    // if !output.status.success() {
-
-    //     let err_msg = str::from_utf8(&output.stderr).unwrap();
-    //     let not_exist_msg = format!("cannot open \'{}\': dataset does not exist\n", &bjdataset);
-
-    //     if err_msg == not_exist_msg {
-    //         // Create the data set
-    //         if let Err(err) = Command::new("zfs").args(&["create", &bjdataset]).status() {
-    //             eprintln!("Error creating data set: {}", &err);
-    //         }
-    //     } else {
-    //         eprintln!("Unexpected error: {}", &err_msg);
-    //         process::exit(1);
-    //     };
-
-    //     // match err {
-    //     //     "dataset does not exist" => println!("no exist"),
-    //     //     _ => println!("big trouble"),
-    //     // }
-    //     // println!("{:?}", &err);
-    // } else {
-    //     println!("Dataset exists already, skipping");
-    // };
-
-    // let cmd = format!("zfs list {}", &bjdataset);
-    // let status = run(&cmd).unwrap().status;
-    // if !status.success() {
-    //     cmd = format!("zfs cateate {}/basejail"
-    // }
-
-
-    // if run(&check_basejail[..]).unwrap().status.success();
-
-    // let out = run("ls", &["/"]).unwrap();
-    // let out = run("ls -la /").unwrap();
-
-    // io::stdout().write_all(&out.stdout).unwrap();
-    // io::stdout().write_all(&out.stderr).unwrap();
-    // println!("{}", &out.status.success());
-    // println!("{}", &out.status.code().unwrap());
 }
