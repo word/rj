9578205ceb507cb615cbb11a80a2333e1318e9a3
Andrew Wasilczuk <akw@scalefactory.com>
2019-12-20 14:51:12 +0000

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use std::process;
 
-mod errors;
 mod cmd;
+mod errors;
 mod zfs;
 
 use reqwest;
-use xz2::read::XzDecoder;
 use std::fs;
-use tar::Archive;
 use std::path::Path;
+use tar::Archive;
+use xz2::read::XzDecoder;
 
 fn main() {
     let j_ds = String::from("zroot/jails");
     let bj_ds = format!("{}/basejail", &j_ds);
     let bj_dir = String::from("/jails/basejail");
     let release = String::from("12.0-RELEASE");
-    let dists = ["base", "lib32" ];
+    let dists = ["base", "lib32"];
 
     for set in vec![&j_ds, &bj_ds] {
         println!("Creating jail data set {}", set);
@@ -23,9 +23,12 @@ fn main() {
             eprintln!("ERROR: {}", err);
             process::exit(1);
         });
-    };
+    }
 
-    let url = format!("http://ftp.uk.freebsd.org/pub/FreeBSD/releases/amd64/amd64/{}/{}.txz", release, dists[1]);
+    let url = format!(
+        "http://ftp.uk.freebsd.org/pub/FreeBSD/releases/amd64/amd64/{}/{}.txz",
+        release, dists[1]
+    );
     let response = reqwest::get(&url).unwrap();
     let decompressor = XzDecoder::new(response);
     let mut archive = Archive::new(decompressor);
@@ -44,5 +47,4 @@ fn main() {
 
         file.unpack_in(&bj_dir).unwrap();
     }
-
 }
