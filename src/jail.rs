4cb570f7181e56ffa06826f70349683a11f9c433
Andrew Wasilczuk <akw@scalefactory.com>
2020-01-25 23:18:01 +0000

    Added Path to test scope

--- src/jail.rs
+++ src/jail.rs
@@ -3,9 +3,6 @@ use anyhow::Result;
 use indexmap::IndexMap;
 use log::info;
 use std::fs;
-use std::path::Path;
-use tar::Archive;
-use xz2::read::XzDecoder;
 
 use crate::settings;
 use crate::source::Source;
@@ -109,36 +106,6 @@ impl Jail<'_> {
     }
 }
 
-// fetch an xz archive using http and extract it to a destination directory
-pub fn fetch_extract(url: &str, dest: &str) -> Result<()> {
-    let response = reqwest::get(url)?;
-    let decompressor = XzDecoder::new(response);
-    let mut archive = Archive::new(decompressor);
-    archive.set_preserve_permissions(true);
-
-    // Remove any matching hard/soft links at the destination before extracting files.  This is
-    // necessary because Tar won't overwrite links and panics instead.  This effectively will
-    // overwrite any existing files and links at the destination.
-    for (_, file) in archive.entries()?.enumerate() {
-        let mut file = file?;
-        let file_dest = Path::new(dest).join(file.path()?);
-
-        // Check if the archived file is a link (hard and soft)
-        if file.header().link_name().unwrap().is_some() {
-            // Check if the link exist at the destination already.  Using symlink_metadata here
-            // becasue is_file returns false for symlinks.  This will match any file, symlink or
-            // hardlink.
-            if file_dest.symlink_metadata().is_ok() {
-                // remove the link so it can be extracted later
-                // println!("removing link {:?}", &file_dest);
-                fs::remove_file(&file_dest)?;
-            }
-        }
-        file.unpack_in(dest)?;
-    }
-    Ok(())
-}
-
 #[cfg(test)]
 mod tests {
 
@@ -147,6 +114,7 @@ mod tests {
     use lazy_static::lazy_static;
     use pretty_assertions::assert_eq;
     use settings::Settings;
+    use std::path::Path;
     use std::sync::Once;
 
     static INIT: Once = Once::new();
