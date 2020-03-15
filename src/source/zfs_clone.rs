11e8e4b61c86b39b6f3324e26814a90c933b3e05
Andrew Wasilczuk <akw@scalefactory.com>
2020-03-15 20:10:07 +0000

    Wrote tests for cloned source

--- src/source/zfs_clone.rs
+++ src/source/zfs_clone.rs
@@ -1,7 +1,6 @@
-use crate::errors::SourceError;
 use crate::jail::Jail;
 use crate::zfs;
-use anyhow::Result;
+use anyhow::{bail, ensure, Result};
 use log::{debug, info};
 use serde::Deserialize;
 
@@ -16,6 +15,15 @@ impl ZfsClone {
         let src_dataset = zfs::DataSet::new(&self.path);
         let dest_dataset = &jail.zfs_ds();
 
+        ensure!(
+            src_dataset.exists()?,
+            format!(
+                "{}: source dataset {} doesn't exist",
+                &jail.name(),
+                &self.path
+            ),
+        );
+
         match src_dataset.last_snap("ready")? {
             Some(snapshot) => {
                 info!(
@@ -29,18 +37,70 @@ impl ZfsClone {
                 Ok(())
             }
             None => {
-                let msg = format!(
-                    "{}: 'ready' snapshot not found in source dataset: {}",
+                bail!(format!(
+                    "{}: 'ready' snapshot not found for source dataset: {}",
                     &jail.name(),
                     &self.path
-                );
-                Err(anyhow::Error::new(SourceError(msg)))
+                ));
             }
         }
     }
 
     pub fn validate(&self) -> Result<()> {
         debug!("Validating zfs clone source");
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::settings::Settings;
+    use crate::zfs::DataSet;
+    use pretty_assertions::assert_eq;
+    use serial_test::serial;
+
+    fn cleanup(source_ds: &DataSet, jail: &Jail) -> Result<()> {
+        jail.destroy()?;
+        if source_ds.exists()? {
+            source_ds.destroy_r()?;
+        }
+        Ok(())
+    }
+
+    #[test]
+    #[serial]
+    fn install() -> Result<()> {
+        let s = Settings::new("testdata/config.toml")?;
+        let jails = s.to_jails()?;
+        let jail = &jails["clone_test"];
+        let source_ds = DataSet::new("zroot/rjtest_clone");
+
+        let clone_source = ZfsClone {
+            path: "zroot/rjtest_clone".to_owned(),
+        };
+
+        cleanup(&source_ds, &jail)?;
+
+        let err = clone_source.install(jail).unwrap_err();
+        assert_eq!(
+            err.downcast::<String>().unwrap(),
+            "clone_test: source dataset zroot/rjtest_clone doesn't exist".to_string()
+        );
+
+        source_ds.create()?;
+
+        let err = clone_source.install(jail).unwrap_err();
+        assert_eq!(
+            err.downcast::<String>().unwrap(),
+            "clone_test: 'ready' snapshot not found for source dataset: zroot/rjtest_clone"
+                .to_string()
+        );
+
+        source_ds.snap("ready")?;
+        clone_source.install(jail)?;
+
+        cleanup(&source_ds, &jail)?;
         Ok(())
     }
 }
